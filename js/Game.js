1d4a154c5221a135255c22100bbfd248a34b9986
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2023-04-16 23:41:16 -0700

    Building constructor classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,65 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+/*
+    startGame() {
+        //"hides the starts screen overlay"
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase() {
+        const randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex]
+    }
+
+    handleInteraction() {
+        "e.target.letter".disable = true;
+        
+        if("phrase does not include guessed letter") {
+            // add `wrong` CSS class to the selected letter's keyboard button
+            removeLife();
+        } else if ("phrase does include guessed letter") {
+            // add `chosen` CSS class to the selected letter's keyboard button
+            this.activePhrase.showMatchedLetter();
+            checkForWin();
+
+            if ("player has won the game") {
+                gameOver();
+            }
+        }
+    }
+
+    removeLife() {
+        "replace 'liveHeart.png' with 'lostHeart.png' in images folder"
+        this.missed += 1
+
+        if (this.missed === 5) {
+            gameOver();
+        }
+    }
+
+    checkForWin() {
+        "checks to see if the player has revealed all of the letters in the active phrase"
+    }
+
+    gameOver() {
+        "display original start screen overlay"
+        if ("win") {
+            "update overlay h1 element with friendly win message"
+        } else "lost" {
+            "update overlay h1 element with loss message"
+        }
+
+        "replace overlay's `start` CSS class with `win` or `lose` CSS class"
+    }
+
+}
+*/
--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+class Game {
+    constructor() {
+
+    }
+}
+
+const resetBtn = document.getElementById("btn__reset");
+
+resetBtn.addEventListener('click', (e) => {
+    new Game();
+    startGame();
+});
+
+const keys = document.querySelectorAll('.key');
+
+for(let i=0; i<keys.length; i++) {
+    keys[i].addEventListener('click', (e) => {
+        game.handleInteraction()
+    });
+}
