e8df7fc728f566347797b91f3a2bdd575741aa6e
Sai Perez <118867950+hsperez6@users.noreply.github.com>
2023-08-04 00:13:37 -0700

    Update app.js
    Changed mario2.PNG

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ startBtn.addEventListener('click', (e) => {
     game = new Game();
     game.startGame();
     const body = document.querySelector('body');
-    body.style.backgroundImage = "url('images/mario2.png')";
+    body.style.backgroundImage = "url('images/mario2.PNG')";
 });
 
 const keyboard = document.querySelector('#qwerty');
